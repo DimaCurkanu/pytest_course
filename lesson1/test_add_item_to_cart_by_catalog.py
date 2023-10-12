731caee8381f544f456ef38fd8ef76557f2c8b6c
DimaCurkanu <kdgrig@gmail.com>
2023-10-12 16:51:33 -0500

    add_item_by_catalog2

--- lesson1/test_add_item_to_cart_by_catalog.py
+++ lesson1/test_add_item_to_cart_by_catalog.py
@@ -5,12 +5,11 @@ def test_add_item_by_card(driver, login_by_standard_user):
 
     add_to_cart_button = driver.find_element(By.CSS_SELECTOR,'#add-to-cart-sauce-labs-bike-light')
     add_to_cart_button.click()
-    # time.sleep(3)
+    time.sleep(2)
 
     go_to_cart = driver.find_element(By.CSS_SELECTOR, '.shopping_cart_link')
     go_to_cart.click()
     # time.sleep(3)
 
     check_cart_text = driver.find_element(By.ID, 'item_0_title_link').text
-    # print(check_cart_text)
     assert check_cart_text == "Sauce Labs Bike Light"
