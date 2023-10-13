6e4c2a8462e3fb0075b1ac9cddb53e8a83431d4c
DimaCurkanu <kdgrig@gmail.com>
2023-10-13 15:01:25 -0500

    open card from picture

--- lesson1/test_redirect_to_card_from_picture.py
+++ lesson1/test_redirect_to_card_from_picture.py
@@ -1,21 +1,13 @@
 from selenium.webdriver.common.by import By
 import time
-from conftest import YOUR_CART_TEXT
 
 def test_open_card_from_picture(driver, login_by_standard_user):
-    item_picture = driver.find_element(By.XPATH, '// img[@alt = "Sauce Labs Bike Light"]')
+    item_picture = driver.find_element(By.XPATH, '//img[@alt = "Sauce Labs Fleece Jacket"]')
     item_picture.click()
-    time.sleep(3)
+    time.sleep(1)
 
-    add_to_cart_button = driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-bike-light')
-    add_to_cart_button.click()
+    card_picture = driver.find_element(By.XPATH, '//img[@alt = "Sauce Labs Fleece Jacket"]')
 
-    go_to_cart = driver.find_element(By.CSS_SELECTOR, '.shopping_cart_link')
-    go_to_cart.click()
-    time.sleep(3)
-
-    yuor_cart = driver.find_element(By.XPATH, '//span[@class = "title"]').text
-
-    assert yuor_cart == YOUR_CART_TEXT
+    assert card_picture.is_displayed()
 
     driver.quit()
