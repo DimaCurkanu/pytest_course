1d4fa247dd5f65ca2a358fe586e1131352c5f835
DimaCurkanu <kdgrig@gmail.com>
2023-10-18 21:34:49 -0500

    test burger menu logout

---
+++ lesson1/test_burger_menu_log_out.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+def test_burger_menu_log_out(driver, login_by_standard_user):
+
+    burger_btn = driver.find_element(By.CSS_SELECTOR, '.bm-burger-button')
+    burger_btn.click()
+
+    logout_btn = driver.find_element(By.ID, 'logout_sidebar_link')
+    logout_btn.click()
+
+    login_btn = driver.find_element(By.ID, 'login-button')
+
+    assert login_btn.is_displayed() is True
+
+    driver.quit()
